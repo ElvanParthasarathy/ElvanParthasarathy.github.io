381d5b5d296d7e0dea0b74510301ee90dba75b7b
Elvan Parthasarathy <144983639+ElvanParthasarathy@users.noreply.github.com>
2025-11-30 12:33:37 +0530

    Create quotes-data.js

---
+++ assets/quotes-data.js
@@ -0,0 +1,70 @@
+// Add NEW quotes at the BOTTOM of this array.
+// Display order is NEWEST FIRST on the page.
+const quotesList = [
+    {
+        original: `“The thing is nowhere — that’s how it goes,
+It’s the plan you make, that no one knows.
+It may change from what it’s meant to be,
+The decision, in the end, it’s all with me.”`,
+        translation: ``,
+        extra: ``,
+        author: "Elvan Parthasarathy"
+    },
+    {
+        original: `"நான் ஒன்றுமறியே,
+மனிதர் முற்றுமறியே.
+எவரும் பிழையற்றோர் அன்று,
+தவரே நிழலாய் நின்றது."`,
+        translation: `“I know not a single knowing,
+nor mankind ever all-knowing.
+We are none ever free of flaw —
+it stands beside us evermore,
+like our reflection on the floor.”`,
+        extra: ``,
+        author: "Elvan Parthasarathy"
+    },
+    {
+        original: `"முன்னே முடிவின் முடிவு மாறின்,
+முன்னே முடிவிலா முடிவு நன்னின்"`,
+        translation: `“A chosen course may twist and bend,
+Yet untold paths bring joys unpenned.”`,
+        extra: ``,
+        author: "Elvan Parthasarathy"
+    },
+    {
+        original: `"நிழல் தேடி நீ செல்கையில்
+எவரும் வாரா நீயே எல் ஞாயிறாக"`,
+        translation: `“As you walk seeking shade, none will come —
+You must become your own blazing sun”`,
+        extra: ``,
+        author: "Elvan Parthasarathy"
+    },
+    {
+        original: `“No two shadows dance the same,
+Try to enjoy the rhythm.”`,
+        translation: ``,
+        extra: ``,
+        author: "Elvan Parthasarathy"
+    },
+    {
+        original: `“Every locked door opens only
+to reveal another passage.”`,
+        translation: ``,
+        extra: ``,
+        author: "Elvan Parthasarathy"
+    },
+    {
+        original: `"நன்னதியில் வாழ்த்துவும் பிறரை,
+இதற்கென்னோர் காரணமில்லை.
+அந்நதில் கிளைக்கும் உணர்வை,
+தருவதற்கோர் வழியுமில்லை."`,
+        translation: `“Wishing someone always feels good,
+doesn’t matter what it’s for.
+Just enjoy spreading a little happiness!”`,
+        extra: `"നന്നതിൽ വാഴ്ത്തുവും പിറരൈ
+ഇതർകെനോർ കാരണമില്ലൈ
+അന്നതിൽ കിളൈക്കും ഉണർവൈ
+തരുവതർകോർ വഴിയുമില്ലൈ"`,
+        author: "Elvan Parthasarathy"
+    }
+];
